78d04610880929a298979ee9f13d2963c49b1548
EggsyOnCode <farzansaqib2003@gmail.com>
2024-07-15 17:07:02 +0500

    minimal account func updated

--- src/MinimalAccount.sol
+++ src/MinimalAccount.sol
@@ -11,14 +11,31 @@ import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "@AA/contracts/core/
 import {IEntryPoint} from "@AA/contracts/interfaces/IEntryPoint.sol";
 
 contract MinimalAccount is IAccount, ECDSA, Ownable {
-    // Errors
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
     error MinimalAccount__OnlyFromEntryPoint();
+    error MinimalAccount__OnlyFromEntryPointOrOwner();
+    error MinimalAccount__CallFailed(bytes);
 
+    /*//////////////////////////////////////////////////////////////
+                               STATE VARS
+    //////////////////////////////////////////////////////////////*/
     IEntryPoint private immutable i_entryPoint;
 
+    /*//////////////////////////////////////////////////////////////
+                               MODIFIERS
+    //////////////////////////////////////////////////////////////*/
     modifier requireOnlyFromEntryPoint() {
         if (msg.sender != address(i_entryPoint)) {
             revert MinimalAccount__OnlyFromEntryPoint();
+        }
+        _;
+    }
+
+    modifier requireOnlyFromEntryPointOrOwner() {
+        if (msg.sender != address(i_entryPoint) && msg.sender != owner()) {
+            revert MinimalAccount__OnlyFromEntryPointOrOwner();
         }
         _;
     }
@@ -27,6 +44,11 @@ contract MinimalAccount is IAccount, ECDSA, Ownable {
         i_entryPoint = IEntryPoint(_entryPoint);
     }
 
+    receive() external payable {}
+
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
         requireOnlyFromEntryPoint
@@ -36,6 +58,20 @@ contract MinimalAccount is IAccount, ECDSA, Ownable {
         // _validateNonce() ==> teh smart contract has to ensure the uniqueness of the nonce; but EntryPoint.sol already handles this for us
         _payPrefund();
     }
+
+    function execute(address dest, uint256 value, bytes calldata functionData)
+        external
+        requireOnlyFromEntryPointOrOwner
+    {
+        (bool success, bytes memory res) = dest.call{value: value}(functionData);
+        if (!success) {
+            revert MinimalAccount__CallFailed(res);
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     /// @dev validation logic is kept simple for now: if the msg.sender is the owner and has signed the msg; then its valid
     function _validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash)
@@ -58,5 +94,13 @@ contract MinimalAccount is IAccount, ECDSA, Ownable {
             (bool suc,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
             suc;
         }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
+
+    function getEntryPoint() external view returns (address) {
+        return address(i_entryPoint);
     }
 }
